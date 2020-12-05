8df70c496417635906b3b2cccb2bde837c1eddaa
jsphwllng <joseph.welling@hotmail.com>
2020-12-05 14:20:52 +0100

    day5 BABY

---
+++ day5/day5.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var seatArray []int
+
+func contains(arr []int, seat int) bool {
+	for _, a := range arr {
+		if a == seat {
+			return true
+		}
+	}
+	return false
+}
+
+func day5() {
+	f, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = strings.Replace(input, "F", "0", -1)
+		input = strings.Replace(input, "L", "0", -1)
+		input = strings.Replace(input, "B", "1", -1)
+		input = strings.Replace(input, "R", "1", -1)
+		binary, _ := strconv.ParseInt(input, 2, 64)
+		seatArray = append(seatArray, int(binary))
+	}
+	for i := 0; i < 256*8; i++ {
+		if !contains(seatArray, i) && contains(seatArray, i+1) && contains(seatArray, i-1) {
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	day5()
+}
